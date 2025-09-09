ffd554af4f807cf9519baba75336304966c16c0f
Deado <113162635+DeadProgrammerr@users.noreply.github.com>
2025-09-10 07:47:25 +0800

    erm idk drew a chess board now trying to implement piece move logic

--- chess/gui.py
+++ chess/gui.py
@@ -3,12 +3,41 @@ import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((640, 640))
 clock = pygame.time.Clock()
 running = True
+reset = True
+drag = False
 dt = 0
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+test_surf = pygame.image.load('gameasset/black_chess_pawn.png').convert_alpha()
+test_rect = test_surf.get_rect(center = (40, 40)) # for x and y plus 80
+current_mouse_coords = ""
+
+def draw_chess_board():
+    for x in range(8):
+        for y in range(8):
+            if(x + y) % 2 == 0:
+                chess_pos_x = x * 80
+                chess_pos_y = y * 80 
+                pygame.draw.rect(screen,'brown',(chess_pos_x, chess_pos_y, 80, 80))
+            else:
+                chess_pos_x = x * 80
+                chess_pos_y = y * 80 
+                pygame.draw.rect(screen, 'white', (chess_pos_x, chess_pos_y, 80, 80))
+                
+def valid_move(mouse_pos): # Walmart version
+    x, y = mouse_pos
+    pawn = True
+    if(pawn):
+        while(x >= 40):
+            print("Hello")
+        return True
+    return True
+    # if(x == 40 and y == 0):
+    #     return
+    # return
 
 while running:
     # poll for events
@@ -16,26 +45,50 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        mouse_pos = pygame.mouse.get_pos()
+        if(event.type == pygame.MOUSEBUTTONDOWN): # Press On Img 
+            if(test_rect.collidepoint(mouse_pos)):
+                print(mouse_pos)
+                drag = True
+        if(event.type == pygame.MOUSEBUTTONUP and drag): # Drop
+            print(mouse_pos)
+            if(valid_move(mouse_pos)):
+                test_rect.center = mouse_pos
+            
+            
+            drag = False
+                
+                
+    if reset:
+        draw_chess_board()
+        reset == False       
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
-
-    pygame.draw.circle(screen, "red", player_pos, 40)
-
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-            player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
-        
-   
     
-
+    screen.blit(test_surf, test_rect)
+    
+    
+ 
+        
+    
+    screen.blit(test_surf, test_rect)
+    # mouse_position = pygame.mouse.get_pos()
+    # # if black_pawn_rect.collidepoint(mouse_position):
+    # #     if(pygame.mouse.get_pressed()) == (True, False, False):
+    # #         print(mouse_position)
+    # #         if(pygame.mouse.get_pressed()) == (False, False, True):
+    # #             print(mouse_position)
+                
+    # if black_pawn_rect.collidepoint(mouse_position):
+    #     # print(mouse_position)
+    #     for event in pygame.event.get():
+    #         if(event.type == pygame.MOUSEBUTTONDOWN):
+    #             if(event.type == pygame.MOUSEBUTTONUP):
+    #                 print("Hello")
+            
+                    
+    
+        
+        
     # flip() the display to put your work on screen
     pygame.display.flip()
 
@@ -105,5 +158,5 @@ pygame.quit()
 #         color = "white" if (row + col) % 2 == 0 else "black"
 #         cell = Label(gui, width=10, height=5, bg=color)
 #         cell.grid(row=row, column=col)
-gui.mainloop()    
+# gui.mainloop()    
 
--- chess/guilearn.py
+++ chess/guilearn.py
@@ -10,6 +10,7 @@ dt = 0
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
+teto_surface = pygame.image.load('D:/CODING/Python/Tutorial/graphics/teto.png')
 direction = None
 
 while running:
@@ -30,28 +31,29 @@ while running:
 
     screen.fill("White")
     
-    pygame.draw.circle(screen, "red", player_pos, 10)
+    # pygame.draw.circle(screen, "red", player_pos, 10)
+    screen.blit(teto_surface, (player_pos.x, player_pos.y))
     
     if direction == "UP":
-        if player_pos.y >= 160:
+        if player_pos.y >= 0: #160
            player_pos.y -= 300 * dt
         else:
-            player_pos.y = 560
+            player_pos.y = 720 #560
     elif direction == "DOWN":
-        if player_pos.y <= 560:
+        if player_pos.y <= 720: #560
            player_pos.y += 300 * dt
         else:
-            player_pos.y = 160
+            player_pos.y = 0 #160
     elif direction == "LEFT":
-        if player_pos.x >= 440:
+        if player_pos.x >= 0: #440
             player_pos.x -= 300 * dt
         else:
-            player_pos.x = 840
+            player_pos.x = 1280 #840
     elif direction == "RIGHT":
-        if player_pos.x <= 840:
+        if player_pos.x <= 1280: #840
             player_pos.x += 300 * dt
         else:
-            player_pos.x = 440
+            player_pos.x = 0 #440
     
 
 
